4eba622218358d2165d8ecf816084814d038ab92
iker.canarias@gmail.com <iker.canarias@gmail.com@1d8ce8e6-0e3a-7114-598d-c37b128816c5>
2013-11-23 14:24:38 +0000

    Changes in grid view header. Added texts in english and some changes in design.

--- trunk/schedulers/src/com/scheduler/dragdrop/TextAdapter.java
+++ trunk/schedulers/src/com/scheduler/dragdrop/TextAdapter.java
@@ -1,6 +1,7 @@
 package com.scheduler.dragdrop;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,10 +9,12 @@ import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.TextView;
 
+import com.scheduler.R;
+
 public class TextAdapter extends BaseAdapter {
 
 	private Context context;
-	static final String[] DAYS_OF_WEEK = new String[] { "L", "M", "X", "J", "V", "S", "D" };
+	static final String[] DAYS_OF_WEEK = new String[] { "Mon.", "Tue.", "Wed.", "Thu.", "Fri.", "Sat.", "Sun." };
 
 	public TextAdapter(Context context) {
 		this.context = context;
@@ -23,12 +26,14 @@ public class TextAdapter extends BaseAdapter {
 
 		if (convertView == null) {
 			tv = new TextView(context);
-			tv.setLayoutParams(new GridView.LayoutParams(85, 20));
+			tv.setLayoutParams(new GridView.LayoutParams(85, 25));
 		} else {
 			tv = (TextView) convertView;
 		}
 		tv.setTextSize(10);
+		tv.setTypeface(null, Typeface.BOLD);
 		tv.setGravity(Gravity.CENTER);
+		tv.setBackgroundResource(R.color.cell_filled_hover);
 		tv.setText(DAYS_OF_WEEK[position]);
 		
 		return tv;
