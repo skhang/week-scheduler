4c0e9cbd79ac22c31164b5e32b2f5423398b2d30
iker.canarias@gmail.com <iker.canarias@gmail.com@1d8ce8e6-0e3a-7114-598d-c37b128816c5>
2013-11-24 22:16:28 +0000

    Today day colored with other color in header of the grid.

--- trunk/schedulers/src/com/scheduler/dragdrop/TextAdapter.java
+++ trunk/schedulers/src/com/scheduler/dragdrop/TextAdapter.java
@@ -1,12 +1,13 @@
 package com.scheduler.dragdrop;
 
+import java.util.Calendar;
+
 import android.content.Context;
 import android.graphics.Typeface;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.GridView;
 import android.widget.TextView;
 
 import com.scheduler.R;
@@ -14,7 +15,7 @@ import com.scheduler.R;
 public class TextAdapter extends BaseAdapter {
 
 	private Context context;
-	static final String[] DAYS_OF_WEEK = new String[] { "Mon.", "Tue.", "Wed.", "Thu.", "Fri.", "Sat.", "Sun." };
+	private static final String[] DAYS_OF_WEEK = new String[] { "Mon.", "Tue.", "Wed.", "Thu.", "Fri.", "Sat.", "Sun." };
 
 	public TextAdapter(Context context) {
 		this.context = context;
@@ -36,22 +37,30 @@ public class TextAdapter extends BaseAdapter {
 		tv.setBackgroundResource(R.color.cell_filled_hover);
 		tv.setText(DAYS_OF_WEEK[position]);
 		
+		if (position == getToday()) {
+			tv.setBackgroundResource(R.color.today);
+		}
+		
 		return tv;
 	}
 
+	private int getToday() {
+		int today = Calendar.getInstance().get(Calendar.DAY_OF_WEEK) - 2;
+		if (today < 0) {
+			today = 6;
+		}
+		return today;
+	}
+	
 	@Override
 	public int getCount() {
 		return DAYS_OF_WEEK.length;
 	}
 
-
-
 	@Override
 	public Object getItem(int arg0) {
 		return DAYS_OF_WEEK[arg0];
 	}
-
-
 
 	@Override
 	public long getItemId(int arg0) {
