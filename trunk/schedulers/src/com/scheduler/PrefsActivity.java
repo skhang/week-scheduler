b26dacf79632bf16ed6a66ac79a8bfb20fd51001
iker.canarias@gmail.com <iker.canarias@gmail.com@1d8ce8e6-0e3a-7114-598d-c37b128816c5>
2013-12-19 15:43:38 +0000

    Added language files.

--- trunk/schedulers/src/com/scheduler/PrefsActivity.java
+++ trunk/schedulers/src/com/scheduler/PrefsActivity.java
@@ -1,20 +1,30 @@
 package com.scheduler;
 
+import java.util.Locale;
+
 import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class PrefsActivity extends PreferenceActivity {
+public class PrefsActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.prefs);
+		
+		getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
 		
 		Preference dialogPreference = (Preference) getPreferenceScreen().findPreference("dialog_preference_about");
 		dialogPreference.setOnPreferenceClickListener(new OnPreferenceClickListener() {
@@ -37,5 +47,39 @@ public class PrefsActivity extends PreferenceActivity {
 		        }
 		    });
 		
+	}
+	
+	public static void updateLanguage(Context context, String idioma) {
+	    if (idioma != null && !"".equals(idioma)) {
+	        Locale locale = new Locale(idioma);
+	        Locale.setDefault(locale);
+	        Configuration config = new Configuration();
+	        config.locale = locale;
+	        context.getResources().updateConfiguration(config, null);
+	    }
+	}
+	
+	@Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+		
+        if (key.equals("language")) {
+        	SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+    		String language = sharedPrefs.getString("language", "Default");
+        	updateLanguage(this, language);
+            restartActivity();
+        }
+    }
+	
+	@Override
+	protected void onPause() {
+	    super.onPause();
+	    // Unregister the listener whenever a key changes
+	    getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+	}
+		
+	private void restartActivity() {
+	    Intent intent = getIntent();
+	    finish();
+	    startActivity(intent);
 	}
 }
