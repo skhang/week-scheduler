a0d60b1408c55e44f3d0972b01783bc8e9143777
iker.canarias@gmail.com <iker.canarias@gmail.com@1d8ce8e6-0e3a-7114-598d-c37b128816c5>
2014-04-01 04:45:47 +0000

    Added license header.

--- trunk/schedulers/src/com/smartweeks/RestoreImagesTask.java
+++ trunk/schedulers/src/com/smartweeks/RestoreImagesTask.java
@@ -1,5 +1,25 @@
 package com.smartweeks;
 
+/*
+ * This file is part of Smart weeks project.
+ * 
+ * Smart weeks is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Smart weeks is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Smart weeks.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Sources: https://code.google.com/p/week-scheduler/
+ * Copyright 2013 Iker Canarias.
+ */
+
 import java.io.ByteArrayOutputStream;
 
 import android.app.ProgressDialog;
